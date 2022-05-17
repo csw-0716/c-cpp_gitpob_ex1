dd06eb94d368b331a3e40118c1dd3fc7adc4b620
csw-0716 <104436436+csw-0716@users.noreply.github.com>
2022-05-17 21:47:34 +0800

    Create ex4-2.cpp

---
+++ ex4-2.cpp
@@ -0,0 +1,14 @@
+int (r)
+print("please input r:\n")
+p=r*2*3.14
+a=r**2*3.14
+if(r>=0 ):
+        printf("周長為",p)
+        printf("面積為",a)
+else
+        printf("輸出提示資訊")
+
+#閏年
+x=input("請輸入年分:")
+if(x % 4 ==0):
+    print("潤年為",x)
