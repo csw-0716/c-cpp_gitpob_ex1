3457843233b5619aba899ba79e0d364d5d324bfd
csw-0716 <104436436+csw-0716@users.noreply.github.com>
2022-05-24 21:12:53 +0800

    Create ex5-1.cpp

---
+++ ex5-1.cpp
@@ -0,0 +1,19 @@
+int main()
+{
+    int a[6]={8,3,6,10,45,12};
+    int i,n;
+    
+    n=sizeof(a)/sizeof(int);
+    for(i=0;i < n;i++){
+        printf("a[%d] = %d\n",i,a[i]);
+    }
+   for()
+    printf("%d",&b[i])
+    
+   for()
+    printf b[i]
+    
+    cout<<"Hello World";
+    
+    return 0;
+}
